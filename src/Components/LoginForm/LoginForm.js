bfd34cf584f21697931ee14c3bd967a6e3daeddb
Nataliya Rozmarynovych <n.rozmarynovych@globallogic.com>
2021-01-14 12:53:21 +0200

    Browser validation for login form

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -6,17 +6,22 @@ import IsAuthorizedContext from '../isAuthorized/IsAuthorized';
 import UserLoginService from '../../services/userLoginService';
 
 class LoginForm extends PureComponent {
-  state = {
-    email: "",
-    password: ""
-  }
-
-  updateField = (fieldName, value) => {
-    this.setState({ [fieldName]: value });
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: "",
+      password: ""
+    }
+    this.passwordRef = React.createRef();
+    this.emailRef = React.createRef();
   }
 
   handleSubmit = (e) => {
     e.preventDefault();
+    this.setState({
+      email: this.emailRef.current.value,
+      password: this.passwordRef.current.value
+    });
     UserLoginService.login(this.state.email, this.state.password)
       .then((user) => {
         if (user) {
@@ -36,22 +41,27 @@ class LoginForm extends PureComponent {
       <form method="post"
         className="loginForm"
         onSubmit={this.handleSubmit} >
-        <Input
-          fieldName="email"
-          label="Email"
-          value={this.state.email}
-          onChange={this.updateField}
-          type="text"
-          className="loginFormInput"
-        />
-        <Input
-          fieldName="password"
-          label="Password"
-          value={this.state.password}
-          onChange={this.updateField}
-          type="password"
-          className="loginFormInput"
-        />
+        <label>Email
+          <input
+            type="text"
+            className="loginFormInput"
+            required={true}
+            pattern={"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"}
+            title={"Invalid email address"}
+            placeholder="email"
+            ref={this.emailRef}
+          />
+        </label>
+        <label>Password
+          <input
+            type="password"
+            className="loginFormInput"
+            required={true}
+            minLength={6}
+            placeholder="password"
+            ref={this.passwordRef}
+          />
+        </label>
         <button
           className="button"
           type="submit"
@@ -64,3 +74,64 @@ class LoginForm extends PureComponent {
 };
 LoginForm.contextType = IsAuthorizedContext;
 export default LoginForm;
+
+
+// class LoginForm extends PureComponent {
+//   state = {
+//     email: "",
+//     password: ""
+//   }
+
+//   updateField = (fieldName, value) => {
+//     this.setState({ [fieldName]: value });
+//   }
+
+//   handleSubmit = (e) => {
+//     e.preventDefault();
+//     UserLoginService.login(this.state.email, this.state.password)
+//       .then((user) => {
+//         if (user) {
+//           this.context.setAuthorized(true);
+//           UserLoginService.setSessionUserEmail(this.state.email);
+//           this.props.history.push('/');
+//         } else {
+//           // show error message
+//           alert("Incorrect email or password")
+//           console.log("auth failed");
+//         }
+//       })
+//   }
+
+//   render() {
+//     return (
+//       <form method="post"
+//         className="loginForm"
+//         onSubmit={this.handleSubmit} >
+//         <Input
+//           fieldName="email"
+//           label="Email"
+//           value={this.state.email}
+//           onChange={this.updateField}
+//           type="text"
+//           className="loginFormInput"
+//         />
+//         <Input
+//           fieldName="password"
+//           label="Password"
+//           value={this.state.password}
+//           onChange={this.updateField}
+//           type="password"
+//           className="loginFormInput"
+//         />
+//         <button
+//           className="button"
+//           type="submit"
+//         >
+//           Login
+//       </button>
+//       </form>
+//     );
+//   }
+// };
+// LoginForm.contextType = IsAuthorizedContext;
+// export default LoginForm;
